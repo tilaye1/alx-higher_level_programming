cb7965108f9600d634dc445ba7e5482740a3067a
tilaye1 <til_mer@yahoo.com>
2022-08-24 14:04:39 +0300

    added

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 = str1 + " " + str2
-print("Welcome to {}!".format(str1))
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = (str[39:66] + str[106:112] + str[0:6])
+print(str)
